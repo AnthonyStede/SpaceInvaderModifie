d0161eadef0d8f8641db9b4ccebb9a700eb753f5
valentin-roche <codixos@gmail.com>
2018-02-06 19:26:34 -0500

    retour du main dans game

--- Space invader/org/newdawn/spaceinvaders/Game.java
+++ Space invader/org/newdawn/spaceinvaders/Game.java
@@ -462,11 +462,6 @@ public class Game extends Canvas {
 	 * @param argv The arguments that are passed into our game
 	 */
 	public static void main(String argv[]) {
-		Game g =new Game();
 
-		// Start the main game loop, note: this method will not
-		// return until the game has finished running. Hence we are
-		// using the actual main thread to run the game.
-		g.gameLoop();
 	}
 }
